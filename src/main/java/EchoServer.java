115d42c29e40495a30bce54ecb58e3390f513fc6
GeroJagar <josegeronimojaramillogarzonj@gmail.com>
2025-09-03 17:39:15 -0500

    1 de la 2 hecho sapeee

--- src/main/java/EchoServer.java
+++ src/main/java/EchoServer.java
@@ -11,13 +11,40 @@ public class EchoServer {
             PrintWriter salida = new PrintWriter(socket.getOutputStream(), true);
 
             String mensaje = entrada.readLine(); // lee mensaje del cliente
-            salida.println("Echo: " + mensaje); // lo reenvía
+            salida.println(tratarMensaje(mensaje)); // lo reenvía
 
             socket.close();
             server.close();
         } catch (Exception e) {
             // normalmente se maneja, pero aquí lo dejamos vacío para simplificación
         }
+    }
+
+    private static String tratarMensaje(String mensaje) {
+        ConversionSN converter = new ConversionSN();
+        String[] partes = mensaje.split(";");
+        String resultado = "";
+        switch(partes[0]) {
+            case "1":
+                resultado = converter.decimalABinario(Integer.parseInt(partes[1]), Integer.parseInt(partes[2]));
+                break;
+            case "2":
+                resultado = ""+converter.binarioADecimal(partes[1]);
+                break;
+            case "3":
+                resultado = converter.decimalAHexadecimal(Integer.parseInt(partes[1]),Integer.parseInt(partes[2]));
+                break;
+            case "4":
+                resultado = ""+converter.hexadecimalADecimal(partes[1]);
+                break;
+            case "5":
+                resultado = converter.binarioAHexadecimal(partes[1],Integer.parseInt(partes[2]));
+                break;
+            case "6":
+                resultado = converter.hexadecimalABinario(partes[1]);
+                break;
+        }
+        return resultado;
     }
 }
 
