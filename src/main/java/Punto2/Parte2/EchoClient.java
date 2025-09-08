779690acd77bfe1fc033aca6a29bfbc1fcaa44c7
GeroJagar <josegeronimojaramillogarzonj@gmail.com>
2025-09-08 16:27:33 -0500

    Punto 2 hecho

---
+++ src/main/java/Punto2/Parte2/EchoClient.java
@@ -0,0 +1,69 @@
+package Punto2.Parte2;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+import java.util.Scanner;
+
+public class EchoClient {
+    static final String menu = "Seleccione una opción" +
+            "\n1.  Convertir decimal a binario" +
+            "\n2.  Convertir binario a decimal" +
+            "\n3.  Convertir decimal a hexadecimal" +
+            "\n4.  Convertir hexadecimal a decimal" +
+            "\n5.  Convertir binario a hexadecimal" +
+            "\n6.  Convertir hexadecimal a binario" +
+            "\n7.  Para salir";
+
+    static final String numeroEleccion = "Digite el numero a convertir";
+
+    static final String numeroBits = "Digite el numero de bits"; //Este hay que usarlo dependiendo de la funcion
+
+    public static void main(String[] args) {
+        while (true){
+            Scanner input = new Scanner(System.in);
+            String mensaje = "";
+            System.out.println(menu);
+            int  opcion = input.nextInt();
+            if (opcion == 7){
+                break;
+            }else{
+                input.nextLine();
+                mensaje = mensaje+opcion+" ";
+                System.out.println(numeroEleccion);
+                String num = input.nextLine();
+                mensaje = mensaje+num+" ";
+                if (opcion == 1 || opcion == 3 || opcion == 5 ){
+                    System.out.println(numeroBits);
+                    int numBits = input.nextInt();
+                    input.nextLine();
+                    mensaje = mensaje+numBits;
+                }
+                System.out.println(mensaje);
+
+                enviarMensajeAlServidor(mensaje);
+            }
+
+        }
+    }
+
+    public static String enviarMensajeAlServidor(String mensaje) {
+        try {
+            Socket socket = new Socket("localhost", 5000);
+
+            PrintWriter salida = new PrintWriter(socket.getOutputStream(), true);
+            BufferedReader entrada = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+
+            salida.println(mensaje); // envía mensaje
+            String respuesta = entrada.readLine(); // recibe respuesta
+
+            socket.close();
+            System.out.println(respuesta);
+            return respuesta;
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+}
---
+++ src/main/java/Punto2/Parte2/EchoServer.java
@@ -0,0 +1,57 @@
+package Punto2.Parte2;
+
+import Punto1.ConversionSN;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class EchoServer {
+    public static void main(String[] args) {
+        try {
+            ServerSocket server = new ServerSocket(5000); // puerto
+            Socket socket = server.accept(); // espera conexión
+
+            BufferedReader entrada = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+            PrintWriter salida = new PrintWriter(socket.getOutputStream(), true);
+
+            String mensaje = entrada.readLine(); // lee mensaje del cliente
+            salida.println(tratarMensaje(mensaje)); // lo reenvía
+
+            socket.close();
+            server.close();
+        } catch (Exception e) {
+            // normalmente se maneja, pero aquí lo dejamos vacío para simplificación
+        }
+    }
+
+    private static String tratarMensaje(String mensaje) {
+        ConversionSN converter = new ConversionSN();
+        String[] partes = mensaje.split(" ");
+        String resultado = "";
+        switch(partes[0]) {
+            case "1":
+                resultado = converter.decimalABinario(Integer.parseInt(partes[1]), Integer.parseInt(partes[2]));
+                break;
+            case "2":
+                resultado = ""+converter.binarioADecimal(partes[1]);
+                break;
+            case "3":
+                resultado = converter.decimalAHexadecimal(Integer.parseInt(partes[1]),Integer.parseInt(partes[2]));
+                break;
+            case "4":
+                resultado = ""+converter.hexadecimalADecimal(partes[1]);
+                break;
+            case "5":
+                resultado = converter.binarioAHexadecimal(partes[1],Integer.parseInt(partes[2]));
+                break;
+            case "6":
+                resultado = converter.hexadecimalABinario(partes[1]);
+                break;
+        }
+        return resultado;
+    }
+}
+
